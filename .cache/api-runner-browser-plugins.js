5948e5812f7d838b4020e39a661a55d1f40b7941
INDEXLIVINGMALL\thanat.ma <cadente.rd@gmail.com>
2019-12-10 11:10:08 +0700

    [james] change conf

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
       plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
       options: {"plugins":[],"maxWidth":590},
     },{
